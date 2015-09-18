c8b7877054d9d39f3a26c4d00080f2e2d02e0d91
Remi Collet <remi@php.net>
2015-09-18 17:25:35 +0200

    Release 1.3.0RC1

--- php_xattr.h
+++ php_xattr.h
@@ -23,7 +23,7 @@
 extern zend_module_entry xattr_module_entry;
 #define phpext_xattr_ptr &xattr_module_entry
 
-#define PHP_XATTR_VERSION "1.3.0dev"
+#define PHP_XATTR_VERSION "1.3.0RC1"
 
 #ifdef PHP_WIN32
 #define PHP_XATTR_API __declspec(dllexport)
